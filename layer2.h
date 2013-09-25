c39c490617a72ad6859450002bbfa82f3bf2052d
Robert Krause <robert@f00l.de>
2013-09-25 08:00:32 +0200

    added documentation to all protocol headers

--- layer2.h
+++ layer2.h
@@ -2,43 +2,52 @@
 // netmate layer2 protocols //
 ///////////////////////////////////////////////////////////////////////////////////
 
-GtkGrid *ethernet_grid(struct ether_header *eth);
-GtkGrid *sll_grid(struct sll_header *sll);
+GtkGrid *ethernet_grid(struct ether_header *eth);	// ethernet
+GtkGrid *sll_grid(struct sll_header *sll);		// ssl (linux cooked)
 
 ///////////////////////////////////////////////////////////////////////////////////
 
 GtkGrid *sll_grid(struct sll_header *sll) {
-  GtkGrid *grid;
-  int x, y;
+  GtkGrid *grid;	// the grid itself
+  int x, y;		// position of next empty grid cell
   char *label;		// label of buttons to set
 
+  // init a new empty grid
   grid = GTK_GRID(gtk_grid_new());
 
+  // set columns to be uniform sized (for better bit size representation)
   gtk_grid_set_column_homogeneous(grid, TRUE);
 
   // allocate memory for button label
   label = malloc(100);
 
+  // build bit indication topic line (0 1 2 .. 31)
   for (x=0; x<32; x++) {
     sprintf(label, "%u", x);
     gtk_grid_attach(grid, gtk_label_new(label), x, 0, 1, 1);
   }
 
+  // set cell pointer to next empty grid cell
   x=0;
   y=1;
 
+  // Packet Type
   sprintf(label, "Packet Type: %u", htons(sll->sll_pkttype));
   append_field(grid, &x, &y, sizeof(sll->sll_pkttype)*8, label);
 
+  // ARP Header Type
   sprintf(label, "ARPHDR_ Type: %u", htons(sll->sll_hatype));
   append_field(grid, &x, &y, sizeof(sll->sll_hatype)*8, label);
 
+  // Link-layer Address Length
   sprintf(label, "Link-layer Address Length: %u", htons(sll->sll_halen));
   append_field(grid, &x, &y, sizeof(sll->sll_halen)*8, label);
 
+  // Link-layer Address
   sprintf(label, "Link-layer Address: %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x", sll->sll_addr[0], sll->sll_addr[1], sll->sll_addr[2], sll->sll_addr[3], sll->sll_addr[4], sll->sll_addr[5], sll->sll_addr[6], sll->sll_addr[7]);
   append_field(grid, &x, &y, sizeof(sll->sll_addr)*8, label);
 
+  // Upper Layer Protocol
   sprintf(label, "Protocol Type: 0x%04x", htons(sll->sll_protocol));
   append_field(grid, &x, &y, sizeof(sll->sll_protocol)*8, label);
 
@@ -48,26 +57,31 @@ GtkGrid *sll_grid(struct sll_header *sll) {
   // show ethernet grid (tab)
   gtk_widget_show_all(GTK_WIDGET(grid));
 
+  // return grid to tab builder
   return(grid);
 }
 
 GtkGrid *ethernet_grid(struct ether_header *eth) {
-  GtkGrid *grid;
-  int x, y;
+  GtkGrid *grid;	// the grid itself
+  int x, y;		// position pointer to next empty grid cell
   char *label;		// label of buttons to set
 
+  // init new empty grid
   grid = GTK_GRID(gtk_grid_new());
 
+  // set columns to be uniform sized (for better bit size representation)
   gtk_grid_set_column_homogeneous(grid, TRUE);
 
   // allocate memory for button label
   label = malloc(100);
 
+  // build bit indication topic line (0 1 2 .. 31)
   for (x=0; x<32; x++) {
     sprintf(label, "%u", x);
     gtk_grid_attach(grid, gtk_label_new(label), x, 0, 1, 1);
   }
 
+  // set cell pointer to next empty grid cell
   x=0;
   y=1;
 
@@ -89,5 +103,6 @@ GtkGrid *ethernet_grid(struct ether_header *eth) {
   // show ethernet grid (tab)
   gtk_widget_show_all(GTK_WIDGET(grid));
 
+  // return grid to tab builder
   return(grid);
 }
--- layer4.h
+++ layer4.h
@@ -3,37 +3,51 @@
 ///////////////////////////////////////////////////////////////////////////////////
 
 GtkGrid *tcp_grid(struct tcphdr *tcp, u_char *options) {
-  GtkGrid *grid;
-  int x,y;
+  GtkGrid *grid;	// the grid itself
   char *label;		// label of buttons to set
+  char *optdata;	// option data
+  int x,y;		// position pointer to next empty grid cell
+  int left;		// bytes left for ipv4 options
+  int optlen;		// length of options field
+  int opttype;		// option type
+  int i;		// loop counter for raw data representation
 
+  // init new empty grid
   grid = GTK_GRID(gtk_grid_new());
 
+  // set columns to be uniform sized (for better bit size representation)
   gtk_grid_set_column_homogeneous(grid, TRUE);
 
   // allocate memory for button label
   label = malloc(100);
 
+  // build bit indication topic line (0 1 2 .. 31)
   for (x=0; x<32; x++) {
     sprintf(label, "%u", x);
     gtk_grid_attach(grid, gtk_label_new(label), x, 0, 1, 1);
   }
 
+  // set cell pointer to next empty grid cell
   x=0;
   y=1;
 
+  // source port
   sprintf(label, "Source Port: %u", htons(tcp->source));
   append_field(grid, &x, &y, sizeof(tcp->source)*8, label);
 
+  // destination port
   sprintf(label, "Destination Port: %u", htons(tcp->dest));
   append_field(grid, &x, &y, sizeof(tcp->dest)*8, label);
 
+  // sequence number
   sprintf(label, "Sequence Number: %u", htonl(tcp->seq));
   append_field(grid, &x, &y, sizeof(tcp->seq)*8, label);
 
+  // acknowledgement number
   sprintf(label, "Acknowledgement Number: %u", htonl(tcp->ack_seq));
   append_field(grid, &x, &y, sizeof(tcp->ack_seq)*8, label);
 
+  // data offset
   sprintf(label, "Data Offset: %u (%u bytes)", tcp->doff, tcp->doff*4);
   append_field(grid, &x, &y, 4, label);
 
@@ -117,53 +131,70 @@ GtkGrid *tcp_grid(struct tcphdr *tcp, u_char *options) {
     append_field(grid, &x, &y, 1, "fin");
   }
 
+  // window size
   sprintf(label, "Window Size: %u", htons(tcp->window));
   append_field(grid, &x, &y, sizeof(tcp->window)*8, label);
 
+  // checksum
   sprintf(label, "Checksum: 0x%04x", htons(tcp->check));
   append_field(grid, &x, &y, sizeof(tcp->check)*8, label);
 
+  // urgent pointer
   sprintf(label, "Urgent Pointer: %u", htons(tcp->urg_ptr));
   append_field(grid, &x, &y, sizeof(tcp->urg_ptr)*8, label);
 
-  int left = (tcp->doff-0x05)*4;
-  int optlen;
-  int opttype;
-  int i;
-  char *optdata;
+  // count bytes of option fields
+  left = (tcp->doff-0x05)*4;
+
+  // progress bytes until no option bytes left
   while (left > 0) {
+    // get option type (first byte)
     opttype = options[0];
 
+    // TODO: switch-case check for option type, the current solution is "dirty"
+
+    // option dependent output (some options dont have a kind field)
     if (opttype == 0x01) {
+      // no operation option (pad option - NO kind field)
       sprintf(label, "Option Kind: 1 (NOP)");
       append_field(grid, &x, &y, 8, label);
 
       optlen = 1;
     } else {
+      // options with kind and length field
 
+      // option kind
       sprintf(label, "Option Kind: %u", opttype);
       append_field(grid, &x, &y, 8, label);
 
+      // option length (INCLUDING type and kind field)
       optlen = options[1];
       sprintf(label, "Option Length: %u", optlen);
       append_field(grid, &x, &y, 8, label);
 
+      // option has additional option data?
       if (optlen > 2) {
 
+        // allocate memory for option data (*2 because of hex representation)
         optdata = malloc(optlen*2);
 
-        for (i=0; i<optlen-2; ++i)
-          sprintf(&optdata[i*2], "%02x", (unsigned int)options[i+2]);
-
+        // print bytes in hex format into array
+        for (i=0; i<optlen-2; ++i) sprintf(&optdata[i*2], "%02x", (unsigned int)options[i+2]);
         optdata[optlen] = 0x00;
-        sprintf(label, "Option Data: 0x%s", optdata);
 
+        // option data field
+        sprintf(label, "Option Data: 0x%s", optdata);
         append_field(grid, &x, &y, (optlen-2)*8, label);
+
+        // free data
         free(optdata);
       }
     }
 
+    // reduce length of field
     left -= optlen;
+
+    // increase pointer to options header
     options = options + optlen;
   }
 
@@ -173,5 +204,6 @@ GtkGrid *tcp_grid(struct tcphdr *tcp, u_char *options) {
   // show ethernet grid (tab)
   gtk_widget_show_all(GTK_WIDGET(grid));
 
+  // pass grid back to tab builder function
   return(grid);
 }
