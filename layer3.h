61d6cd5e026f9abf3a7744d64c7aa80972279ed5
1b51a5af78 <creeq@f00l.de>
2013-09-28 07:07:04 +0200

    added ARP protocol

--- layer3.h
+++ layer3.h
@@ -3,6 +3,7 @@
 ///////////////////////////////////////////////////////////////////////////////////
 
 GtkGrid *ipv4_grid(struct iphdr *ipv4, u_char *options);	// ipv4 (type 0x0800)
+GtkGrid *arp_grid(struct arphdr *arp, u_char *options);		// arp (type 0x0806)
 
 ///////////////////////////////////////////////////////////////////////////////////
 
@@ -158,6 +159,80 @@ GtkGrid *ipv4_grid(struct iphdr *ipv4, u_char *options) {
     // increase pointer to options header
     options = options + optlen;
   }
+
+  // free memory of label
+  free(label);
+
+  // show ethernet grid (tab)
+  gtk_widget_show_all(GTK_WIDGET(grid));
+
+  // pass grid back to tab builder function
+  return(grid);
+}
+
+GtkGrid *arp_grid(struct arphdr *arp, u_char *options) {
+  GtkGrid *grid;	// the grid itself
+  char *label;		// label of buttons to set
+  int x,y;		// position pointer to next empty grid cell
+
+  // init new empty grid
+  grid = GTK_GRID(gtk_grid_new());
+
+  // set columns to be uniform sized (for better bit size representation)
+  gtk_grid_set_column_homogeneous(grid, TRUE);
+
+  // allocate memory for button label
+  label = malloc(100);
+
+  // build bit indication topic line (0 1 2 .. 31)
+  for (x=0; x<32; x++) {
+    sprintf(label, "%u", x);
+    gtk_grid_attach(grid, gtk_label_new(label), x, 0, 1, 1);
+  }
+
+  // set cell pointer to next empty grid cell
+  x=0;
+  y=1;
+
+  // hardware type
+  sprintf(label, "Hardware Type: %u", htons(arp->ar_hrd));
+  append_field(grid, &x, &y, sizeof(arp->ar_hrd)*8, label);
+
+  // protocol type
+  sprintf(label, "Protocol Type: 0x%04x", htons(arp->ar_pro));
+  append_field(grid, &x, &y, sizeof(arp->ar_pro)*8, label);
+
+  // hardware length
+  sprintf(label, "Hardware Length: %u", arp->ar_hln);
+  append_field(grid, &x, &y, sizeof(arp->ar_hln)*8, label);
+
+  // protocol length
+  sprintf(label, "Protocol Length: %u", arp->ar_pln);
+  append_field(grid, &x, &y, sizeof(arp->ar_pln)*8, label);
+
+  // operation
+  sprintf(label, "Operation: %u", htons(arp->ar_op));
+  append_field(grid, &x, &y, sizeof(arp->ar_op)*8, label);
+
+  // sender hardware address (SHA)
+  sprintf(label, "Sender Hardware Address: %02x:%02x:%02x:%02x:%02x:%02x", options[0], options[1], options[2], options[3], options[4], options[5]);
+  append_field(grid, &x, &y, 48, label);
+  options += 6;
+
+  // sender protocol address (SPA)
+  sprintf(label, "Sender Protocol Address: %u.%u.%u.%u", options[0], options[1], options[2], options[3]);
+  append_field(grid, &x, &y, 32, label);
+  options += 4;
+
+  // sender hardware address (THA)
+  sprintf(label, "Target Hardware Address: %02x:%02x:%02x:%02x:%02x:%02x", options[0], options[1], options[2], options[3], options[4], options[5]);
+  append_field(grid, &x, &y, 48, label);
+  options += 6;
+
+  // sender protocol address (TPA)
+  sprintf(label, "Target Protocol Address: %u.%u.%u.%u", options[0], options[1], options[2], options[3]);
+  append_field(grid, &x, &y, 32, label);
+  options += 4;
 
   // free memory of label
   free(label);
