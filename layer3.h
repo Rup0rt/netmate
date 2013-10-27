b3f51b8ef9a83555b66bccaa12c254b5b9a86c5b
Robert Krause <Ruport@web.de>
2013-10-27 14:45:43 +0100

    added tooltips for ipv4 and ipv6

--- layer3.h
+++ layer3.h
@@ -44,75 +44,75 @@ GtkGrid *ipv4_grid(struct iphdr *ipv4, u_char *options) {
 
   /* read and set ip version field */
   sprintf(label, "Version: %u", ipv4->version);
-  append_field(grid, &x, &y, 4, label, NULL);
+  append_field(grid, &x, &y, 4, label, IPV4_VERSION);
 
   /* read and set ip header length (<< 2 to calculate real size) */
   sprintf(label, "IHL: %u (%u bytes)", ipv4->ihl, ipv4->ihl*4);
-  append_field(grid, &x, &y, 4, label, NULL);
+  append_field(grid, &x, &y, 4, label, IPV4_IHL);
 
   /* read and set ip dscp field */
   ipv4_dscp = ipv4->tos >> 2;
   sprintf(label, "DSCP: 0x%02x", ipv4_dscp);
-  append_field(grid, &x, &y, 6, label, NULL);
+  append_field(grid, &x, &y, 6, label, IPV4_DCSP);
 
   /* read and set ip ecn field */
   ipv4_ecn = ipv4->tos & 0x03;
   sprintf(label, "ECN:\n0x%02x", ipv4_ecn);
-  append_field(grid, &x, &y, 2, label, NULL);
+  append_field(grid, &x, &y, 2, label, IPV4_ECN);
 
   /* read and set total length of ip header */
   sprintf(label, "Total Length: %u", htons(ipv4->tot_len));
-  append_field(grid, &x, &y, sizeof(ipv4->tot_len)*8, label, NULL);
+  append_field(grid, &x, &y, sizeof(ipv4->tot_len)*8, label, IPV4_TOTLEN);
 
   /* read and set identification field of ip packet */
   sprintf(label, "Identification: 0x%04x", htons(ipv4->id));
-  append_field(grid, &x, &y, sizeof(ipv4->id)*8, label, NULL);
+  append_field(grid, &x, &y, sizeof(ipv4->id)*8, label, IPV4_IDENTIFICATION);
 
   /* reserved flag */
   if (ipv4->frag_off & htons(IP_RF)) {
-    append_field(grid, &x, &y, 1, "RF", NULL);
+    append_field(grid, &x, &y, 1, "RF", IPV4_FLAG_RESERVED);
   } else {
-    append_field(grid, &x, &y, 1, "rf", NULL);
+    append_field(grid, &x, &y, 1, "rf", IPV4_FLAG_RESERVED);
   }
 
   /* dont fragment flag */
   if (ipv4->frag_off & htons(IP_DF)) {
-    append_field(grid, &x, &y, 1, "DF", NULL);
+    append_field(grid, &x, &y, 1, "DF", IPV4_FLAG_DF);
   } else {
-    append_field(grid, &x, &y, 1, "df", NULL);
+    append_field(grid, &x, &y, 1, "df", IPV4_FLAG_DF);
   }
 
   /* more fragments flag */
   if (ipv4->frag_off & htons(IP_MF)) {
-    append_field(grid, &x, &y, 1, "MF", NULL);
+    append_field(grid, &x, &y, 1, "MF", IPV4_FLAG_MF);
   } else {
-    append_field(grid, &x, &y, 1, "mf", NULL);
+    append_field(grid, &x, &y, 1, "mf", IPV4_FLAG_MF);
   }
 
   /* read and set ip fragmentation offset (<< 3 to calculate real size); */
   ipv4_offset = (htons(ipv4->frag_off) & IP_OFFMASK);
   sprintf(label, "Fragment Offset: %u (%u bytes)", ipv4_offset, ipv4_offset << 3);
-  append_field(grid, &x, &y, 13, label, NULL);
+  append_field(grid, &x, &y, 13, label, IPV4_FRAGOFF);
 
   /* read and set time to live of ip packet */
   sprintf(label, "Time To Live: %u", ipv4->ttl);
-  append_field(grid, &x, &y, sizeof(ipv4->ttl)*8, label, NULL);
+  append_field(grid, &x, &y, sizeof(ipv4->ttl)*8, label, IPV4_TTL);
 
   /* read an d set upper layer protocol */
   sprintf(label, "Protocol: %u", ipv4->protocol);
-  append_field(grid, &x, &y, sizeof(ipv4->protocol)*8, label, NULL);
+  append_field(grid, &x, &y, sizeof(ipv4->protocol)*8, label, IPV4_PROTOCOL);
 
   /* read and set ip header checksum */
   sprintf(label, "Header checksum: 0x%04x", htons(ipv4->check));
-  append_field(grid, &x, &y, sizeof(ipv4->check)*8, label, NULL);
+  append_field(grid, &x, &y, sizeof(ipv4->check)*8, label, IPV4_CHECKSUM);
 
   /* read and set ip source address */
   sprintf(label, "Source IP Address: %u.%u.%u.%u", ipv4->saddr & 0xff, (ipv4->saddr >> 8) & 0xff, (ipv4->saddr >> 16) & 0xff, (ipv4->saddr >> 24) & 0xff);
-  append_field(grid, &x, &y, sizeof(ipv4->saddr)*8, label, NULL);
+  append_field(grid, &x, &y, sizeof(ipv4->saddr)*8, label, IPV4_SOURCE);
 
   /* read and set ip destination address */
   sprintf(label, "Destination IP Address: %u.%u.%u.%u", ipv4->daddr & 0xff, (ipv4->daddr >> 8) & 0xff, (ipv4->daddr >> 16) & 0xff, (ipv4->daddr >> 24) & 0xff);
-  append_field(grid, &x, &y, sizeof(ipv4->daddr)*8, label, NULL);
+  append_field(grid, &x, &y, sizeof(ipv4->daddr)*8, label, IPV4_DESTINATION);
 
   /* count bytes of option fields */
   left = (ipv4->ihl-0x05)*4;
@@ -124,23 +124,23 @@ GtkGrid *ipv4_grid(struct iphdr *ipv4, u_char *options) {
 
     /* copy bit (bit 1) */
     if (opttype & IPOPT_COPY) {
-      append_field(grid, &x, &y, 1, "C", NULL);
+      append_field(grid, &x, &y, 1, "C", IPV4_OPTION_FLAG_COPIED);
     } else {
-      append_field(grid, &x, &y, 1, "c", NULL);
+      append_field(grid, &x, &y, 1, "c", IPV4_OPTION_FLAG_COPIED);
     }
 
     /* option class (bit 2 & 3) */
     sprintf(label, "Class: %u", opttype & IPOPT_CLASS_MASK);
-    append_field(grid, &x, &y, 2, label, NULL);
+    append_field(grid, &x, &y, 2, label, IPV4_OPTION_CLASS);
 
     /* option number (bit 4-8) */
     sprintf(label, "Number: %u", opttype & IPOPT_NUMBER_MASK);
-    append_field(grid, &x, &y, 5, label, NULL);
+    append_field(grid, &x, &y, 5, label, IPV4_OPTION_NUMBER);
 
     /* options length (INCLUDING type & length fields) */
     optlen = options[1];
     sprintf(label, "Length: %u", optlen);
-    append_field(grid, &x, &y, 8, label, NULL);
+    append_field(grid, &x, &y, 8, label, IPV4_OPTION_LENGTH);
 
     /* allocate memory for option data (*2 because of hex representation) */
     optdata = malloc(optlen*2);
@@ -151,7 +151,7 @@ GtkGrid *ipv4_grid(struct iphdr *ipv4, u_char *options) {
 
     /* option data field */
     sprintf(label, "Opt. Data 0x%s", optdata);
-    append_field(grid, &x, &y, (optlen-2)*8, label, NULL);
+    append_field(grid, &x, &y, (optlen-2)*8, label, IPV4_OPTION_DATA);
 
     /* free data */
     free(optdata);
@@ -210,60 +210,60 @@ GtkGrid *ipv6_grid(struct ip6_hdr *ipv6, u_char *options) {
   /* read and set ip version field */
   ipv6_version = htonl(ipv6->ip6_ctlun.ip6_un1.ip6_un1_flow) >> 28;
   sprintf(label, "Version: %u", ipv6_version);
-  append_field(grid, &x, &y, 4, label, NULL);
+  append_field(grid, &x, &y, 4, label, IPV6_VERSION);
 
   /* traffic class */
   ipv6_tc = htonl(ipv6->ip6_ctlun.ip6_un1.ip6_un1_flow) >> 20 & 0x00ff;
   sprintf(label, "Traffic Class: 0x%02x", ipv6_tc);
-  append_field(grid, &x, &y, 8, label, NULL);
+  append_field(grid, &x, &y, 8, label, IPV6_TC);
 
   /* flow label */
   ipv6_fl = htonl(ipv6->ip6_ctlun.ip6_un1.ip6_un1_flow) & 0x000fffff;
   sprintf(label, "Flow Label: 0x%06x", ipv6_fl);
-  append_field(grid, &x, &y, 20, label, NULL);
+  append_field(grid, &x, &y, 20, label, IPV6_FLOW);
 
   /* payload length */
   sprintf(label, "Payload Length: %u", htons(ipv6->ip6_ctlun.ip6_un1.ip6_un1_plen));
-  append_field(grid, &x, &y, sizeof(ipv6->ip6_ctlun.ip6_un1.ip6_un1_plen)*8, label, NULL);
+  append_field(grid, &x, &y, sizeof(ipv6->ip6_ctlun.ip6_un1.ip6_un1_plen)*8, label, IPV6_PAYLEN);
 
   /* next header */
   ipv6_nh = ipv6->ip6_ctlun.ip6_un1.ip6_un1_nxt;
   sprintf(label, "Next Header: %u", ipv6_nh);
-  append_field(grid, &x, &y, sizeof(ipv6->ip6_ctlun.ip6_un1.ip6_un1_nxt)*8, label, NULL);
+  append_field(grid, &x, &y, sizeof(ipv6->ip6_ctlun.ip6_un1.ip6_un1_nxt)*8, label, IPV6_NEXT_HEADER);
 
   /* hop limit */
   sprintf(label, "Hop Limit: %u", ipv6->ip6_ctlun.ip6_un1.ip6_un1_hlim);
-  append_field(grid, &x, &y, sizeof(ipv6->ip6_ctlun.ip6_un1.ip6_un1_hlim)*8, label, NULL);
+  append_field(grid, &x, &y, sizeof(ipv6->ip6_ctlun.ip6_un1.ip6_un1_hlim)*8, label, IPV6_HOP_LIMIT);
 
   /* source address */
   sprintf(label, "Source Address: %04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x", htons(ipv6->ip6_src.__in6_u.__u6_addr16[0]), htons(ipv6->ip6_src.__in6_u.__u6_addr16[1]), htons(ipv6->ip6_src.__in6_u.__u6_addr16[2]), htons(ipv6->ip6_src.__in6_u.__u6_addr16[3]), htons(ipv6->ip6_src.__in6_u.__u6_addr16[4]), htons(ipv6->ip6_src.__in6_u.__u6_addr16[5]), htons(ipv6->ip6_src.__in6_u.__u6_addr16[6]), htons(ipv6->ip6_src.__in6_u.__u6_addr16[7]));
-  append_field(grid, &x, &y, 128, label, NULL);
+  append_field(grid, &x, &y, 128, label, IPV6_SOURCE);
 
   /* destination address */
   sprintf(label, "Destination Address: %04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x", htons(ipv6->ip6_dst.__in6_u.__u6_addr16[0]), htons(ipv6->ip6_dst.__in6_u.__u6_addr16[1]), htons(ipv6->ip6_dst.__in6_u.__u6_addr16[2]), htons(ipv6->ip6_dst.__in6_u.__u6_addr16[3]), htons(ipv6->ip6_dst.__in6_u.__u6_addr16[4]), htons(ipv6->ip6_dst.__in6_u.__u6_addr16[5]), htons(ipv6->ip6_dst.__in6_u.__u6_addr16[6]), htons(ipv6->ip6_dst.__in6_u.__u6_addr16[7]));
-  append_field(grid, &x, &y, 128, label, NULL);
+  append_field(grid, &x, &y, 128, label, IPV6_DESTINATION);
 
   while (ipv6_nh == IPPROTO_HOPOPTS) {
     /* next header */
     ipv6_nh = options[0];
     sprintf(label, "Next Header: %u", ipv6_nh);
-    append_field(grid, &x, &y, sizeof(ipv6->ip6_ctlun.ip6_un1.ip6_un1_nxt)*8, label, NULL);
+    append_field(grid, &x, &y, sizeof(ipv6->ip6_ctlun.ip6_un1.ip6_un1_nxt)*8, label, IPV6_NEXT_HEADER);
 
     /* options length */
     hoplen = options[1];
     sprintf(label, "Length: %u (%u bytes)", hoplen, hoplen*8);
-    append_field(grid, &x, &y, 8, label, NULL);
+    append_field(grid, &x, &y, 8, label, IPV6_HDR_EXT_LEN);
     options += 2;
 
     left = hoplen*8 + 6;
     while (left > 0) {
       opttype = options[0];
       sprintf(label, "Type: %u", opttype);
-      append_field(grid, &x, &y, 8, label, NULL);
+      append_field(grid, &x, &y, 8, label, IPV6_OPTION_TYPE);
 
       optlen = options[1];
       sprintf(label, "Length: %u", optlen);
-      append_field(grid, &x, &y, 8, label, NULL);
+      append_field(grid, &x, &y, 8, label, IPV6_OPTION_LENGTH);
 
       if (optlen > 0) {
         optdata = malloc(optlen*2+1);
@@ -272,7 +272,7 @@ GtkGrid *ipv6_grid(struct ip6_hdr *ipv6, u_char *options) {
         optdata[optlen*2] = 0x00;
 
         sprintf(label, "Data: 0x%s", optdata);
-        append_field(grid, &x, &y, optlen*8, label, NULL);
+        append_field(grid, &x, &y, optlen*8, label, IPV6_OPTION_DATA);
 
         free(optdata);
       }
